a40511f26571b81866f0cd0ab94ef4a34ec21e79
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2020-04-13 13:26:54 +0600

    Add all files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+//Detecting HTML Button Press
+
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
+while (noOfDrumButtons--) {
+    document.querySelectorAll(".drum")[noOfDrumButtons].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+//Detecting Keyboard Press
+//Function parameter's name can be anything as your wish event/e/evt/e etc.
+
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key); 
+    buttonAnimation(event.key);   
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "a":
+            var kickbass = new Audio("sounds/kick-bass.mp3");
+            kickbass.play();
+            break;
+
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        default:
+            console.log(buttonInnerHTML);
+            break;
+    }    
+}
+
+//Creating button animation
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {       
+        activeButton.classList.remove("pressed");       
+    }, 100);
+}
